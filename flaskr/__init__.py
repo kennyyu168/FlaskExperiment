03604405ac20a253dee2f9c928de550ec5d25f3f
Kenny Yu <kennyyu168@yahoo.com>
2019-07-28 19:07:48 -0700

    Created an SQLite database, created a simple webpage

---
+++ flaskr/__init__.py
@@ -0,0 +1,50 @@
+# Filename: __init__.py
+# Author: Kenny Yu
+# Date: 7/28/19
+
+# Import 
+import os
+
+# Import flask functions
+from flask import Flask
+
+# Function Name: create_app()
+# Function Description: Application factory function, once it's called, it
+#     sets up and configures the app.
+def create_app ( test_config=None ):
+	
+	# Create and configure app
+	app = Flask( __name__, instance_relative_config=True )
+	app.config.from_mapping( SECRET_KEY='dev', 
+	    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite' ), )
+	
+	# If there isn't a test config
+	if test_config is None:
+		
+		# Load the instance config, if it exists, when not testing
+		app.config.from_pyfile( 'config.py', silent=True )
+
+	else: 
+		
+		# Load the test config if passed in
+		app.config.from_mapping( test_config )
+
+	# Ensure the instance folder exists
+	try:
+		os.makedirs( app.instance_path )
+	except OSError:
+		pass
+
+	# a simple page that says hello
+	@app.route('/hello')
+	def hello():
+		return 'Hello, World!'
+
+	# Brings in functions from the db.py file
+	from . import db
+
+	# Initializes the app
+	db.init_app( app )
+
+	return app
+
