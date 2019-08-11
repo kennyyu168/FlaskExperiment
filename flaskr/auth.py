46bc5b5ae7df09bcbb10346031ce1f159475d3f1
Kenny Yu <kennyyu168@yahoo.com>
2019-08-11 14:10:20 -0700

    Added a login, logout, and function login_required to check for the status of logins

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -69,5 +69,31 @@ def login():
 
 	return render_template( 'auth/login.html' )
 
+# Function Name: load_logged_in_user()
+# Function Description: 
+@bp.before_app_request
+def load_logged_in_user():
+	user_id = session.get( 'user_id' )
 
+	if user_id is None:
+		g.user = None
+	else:
+		g.user = get_db().execute( 'SELECT * FROM user WHERE id = ?', 
+		    (user_id,) ).fetchone()
 
+# Function Name: logout()
+# Function Description: 
+@bp.route( '/logout' )
+def logout():
+	session.clear()
+	return redirect( url_for( 'index' ) )
+
+# Function Name: login_required()
+# Function Description: 
+def login_required( view ): 
+	@functools.wraps(view)
+	def wrapped_view(**kwargs):
+		if g.user is None:
+			return redirect( url_for( 'auth.login' ) )
+		return view(**kwargs)
+	return wrapped_view
