10c0cd1909483dfd39802fc8218028fb19f684e2
Kenny Yu <kennyyu168@yahoo.com>
2019-08-23 20:41:31 -0700

    Finished the Tutorial, will be adding new features to experiment

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -29,8 +29,8 @@ def register():
 			error = 'Username is required.'
 		elif not password:
 			error = 'Password is required.'
-		elif db.execute('SELECT if FROM user WHERE username = ?', 
-		     (username,) ).fetchone() is not NONE:
+		elif db.execute('SELECT id FROM user WHERE username = ?', 
+		    (username,) ).fetchone() is not None:
 		     error = 'User {} is already registered.'.format(username)
 
 		if error is None:
@@ -53,7 +53,7 @@ def login():
 		db = get_db()
 		error = None
 		user = db.execute( 'SELECT * FROM user WHERE username = ?', 
-		    (username,).fetchone()
+		    (username,)).fetchone()
 
 		if user is None:
 			error = 'Incorrect username.'
